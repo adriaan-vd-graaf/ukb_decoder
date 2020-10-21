863aea45e3fd821f583be45f7299e6a2fa7a0ffa
adriaan <adriaan.vd.graaf@gmail.com>
2020-10-21 16:27:55 +0200

    bugfix

--- ukb_decoder/__init__.py
+++ ukb_decoder/__init__.py
@@ -158,8 +158,9 @@ class decoder:
                     if np.isnan(value):
                         continue #leave it as is.
                     elif  value not in self.decoder_to_ordinal.keys():
-                        raise ValueError(f"{field_of_interest.field_id} did not contain the value {value} in the "
+                        print(f"{field_of_interest.field_id} did not contain the value {value} in the "
                                          f"set: {self.decoder_to_ordinal[field_coding].keys()}")
+                        raise ValueError("Incorrect value found, see above")
                     else:
                         tmp_data_vec[i] = self.decoder_to_ordinal[field_coding][value]
 
