4bbea055f6069e178b4aabc586be7e8ae10e4503
adriaan <adriaan.vd.graaf@gmail.com>
2021-02-01 17:22:31 +0100

    UKBICD10Pheno class debugging

--- ukb_decoder/field_phenotype.py
+++ ukb_decoder/field_phenotype.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from . decoder import *
 import icd10
+import re
 import datetime
 from collections import defaultdict
 
@@ -188,7 +189,8 @@ class UKBICD10Pheno(PhenotypesPerField):
         """
         self.indice_and_diagnoses = {i : {} for i in range(self.n_participants)}
 
-        self.indice_and_birthdates = {}
+        self.birthdates = {}
+
 
         self.diagnoses_count = defaultdict(int)
 
@@ -205,6 +207,8 @@ class UKBICD10Pheno(PhenotypesPerField):
         birthdates = {i: datetime.date(year=int(float(year_dat[i])), month=int(float(months_dat[i])), day=1) for i in
                       range(self.n_participants) if year_dat[i] != 'nan' and months_dat[i] != 'nan'}
 
+        self.birthdates = birthdates
+
         return birthdates
 
 
@@ -212,26 +216,25 @@ class UKBICD10Pheno(PhenotypesPerField):
                                    diagnosis_series: pd.DataFrame,
                                    date_of_diagnosis_series: pd.DataFrame, month_year_of_birth: dict):
 
-        diagnosis_list = diagnosis_series.astype(str).tolist() # This makes the nan values a string of 'nan'
-        date_list = [str(x).split('-') for x in date_of_diagnosis_series.astype(str).tolist()]
-        date_list = [datetime.date(year=int(x[0]), month=int(x[1]), day=int(x[2])) if x[0] != 'nan' else 'nan' for x in date_list]
-        days_since_diagnosis = [(x - month_year_of_birth[i]).days
-                                if (x != 'nan' and month_year_of_birth[i] != 'nan') else 'nan'
-                                for i, x in enumerate(date_list)]
+        diagnosis_dict = {i : x for i, x in enumerate(diagnosis_series.astype(str).tolist()) if x != 'nan'} # This makes the nan values a string of 'nan'
+        date_dict = {i: str(x).split('-') for i, x in enumerate(date_of_diagnosis_series.astype(str).tolist()) if x != 'nan'}
+        date_dict = {i: datetime.date(year=int(x[0]), month=int(x[1]), day=int(x[2])) for i, x in date_dict.items()}
+        days_since_diagnosis = {i: (x - month_year_of_birth[i]).days for i, x in date_dict.items()
+                                if month_year_of_birth[i] != 'nan'}
         
-        for i in range(self.n_participants):
-            if diagnosis_list[i] == 'nan':
-                if days_since_diagnosis[i] != 'nan':
-                    print(f'Warning, Found a date of diagnosis, without a ICD10 code on line {i}, {instance}.{array}, date was {date_list[i]}, continueing.')
+        for i in set(diagnosis_dict.keys()) | set(days_since_diagnosis.keys()):
+            if i not in diagnosis_dict.keys():
+                if i not in days_since_diagnosis.keys() :
+                    print(f'Warning, Found a date of diagnosis, without a ICD10 code on line {i}, {instance}.{array}, date was {date_dict[i]}, continueing.')
                 continue
 
             else:
-                if days_since_diagnosis[i] == 'nan' and month_year_of_birth[i] != 'nan':
+                if i not in days_since_diagnosis.keys() == 'nan' and month_year_of_birth[i] != 'nan':
                     print(
-                        f'Found an ICD10 code diagnosis, without a date line {i}, {instance}.{array}, ICD10code was {diagnosis_list[i]}, continueing.')
+                        f'Found an ICD10 code diagnosis, without a date line {i}, {instance}.{array}, ICD10code was {diagnosis_dict[i]}, continueing.')
                     continue
-                self.indice_and_diagnoses[i][diagnosis_list[i]] = days_since_diagnosis[i]
-                self.diagnoses_count[diagnosis_list[i]] += 1
+                self.indice_and_diagnoses[i][diagnosis_dict[i]] = days_since_diagnosis[i]
+                self.diagnoses_count[diagnosis_dict[i]] += 1
 
         self.phenotypes_added[self._index(instance, array)] = True
 
@@ -257,6 +260,89 @@ class UKBICD10Pheno(PhenotypesPerField):
                                          pandas_df[f'{self.dates_of_diagnosis_field.field_id}-{instance}.{array}'],
                                          month_year_of_birth
                                          )
+
+
+    def make_cc_and_date_pheno_mat(self, include: set, exclude=None, regex=False, exclude_from_cases=False,
+                       date_to_compare = datetime.date(year=2020, month=11, day=30) ):
+        """
+        Will return an (self.n_individuals, 2) shaped matrix containing the case status in the first column
+        The age will be in the second column if the individual is a control.
+        otherwise, the days since birth of diagnosis is taken.
+
+        :param include:
+        :param exclude: a set that will exclude individual with certain traits.
+                        if the an individual is a case,
+                        default=None
+        :param regex: if a regex is used to match from include and exclude set
+        :param exclude_from_cases: if a case also has an exclusion factor present
+
+        :param date_to_compare: is a datetime.date object that will be used to identify days lived.
+                                As of Feb 1. 2021, the last known ICD10 code inclusion for ukb was done at this date
+
+        :return: a vector of stuff
+        """
+        if not self.all_phenotypes_present():
+            raise ValueError("All phenotypes need to be present before a case control phenotype can be made")
+
+        if exclude is None:
+            exclude = set()
+
+
+        phenotype_matrix = np.zeros((self.n_participants, 2), dtype=float)
+
+        if regex:
+            # This will first search for all diagnosis codes that match.
+            # So that the matching code happens on a set in both cases.
+
+            include_regexes = [re.compile(x) for x in include]
+            exclude_regexes = [re.compile(x) for x in exclude]
+            to_include = set()
+            to_exclude = set()
+
+            for icd10_code in self.diagnoses_count.keys(): # self.diagnoses_and_count contains all diagnoses
+
+                for regex in include_regexes:
+                    if regex.match(icd10_code):
+                        to_include.add(icd10_code)
+
+                for regex in exclude_regexes:
+                    if regex.match(icd10_code):
+                        to_exclude.add(icd10_code)
+
+        else:
+            to_include = include
+            to_exclude = exclude
+
+        # Now iterate over all the individuals, and identify the earliest match, use that as a date.
+        for indice in range(self.n_participants):
+
+            include_matches = to_include.intersection(self.indice_and_diagnoses[indice].keys())
+            exclude_matches = to_exclude.intersection(self.indice_and_diagnoses[indice].keys())
+
+            # the individual has both include and exclude diagnosis and this is not allowed.
+            if exclude_from_cases and len(include_matches.intersection(exclude_matches)) > 0:
+                phenotype_matrix[indice,:] = np.nan
+            # the individual has an include match
+            elif len(include_matches) > 0:
+                #we are dealing with a case here.
+                phenotype_matrix[indice, 0] = 1.0
+                phenotype_matrix[indice, 1] = np.min([float(self.indice_and_diagnoses[indice][x]) for x in include_matches])
+
+            # the individual has an exclude match
+            elif len(exclude_matches) > 0 :
+                phenotype_matrix[indice,:] = np.nan
+
+            #this is when the individuals is a bona-fide control
+            else:
+                try:
+                    day_difference = (date_to_compare - self.birthdates[indice]).days
+                    phenotype_matrix[indice, 0] = 0.0
+                    phenotype_matrix[indice, 1] = day_difference
+                except KeyError: #this can happen when the birthmonth and year is not present.
+                    phenotype_matrix[indice, :] = np.nan
+
+        return phenotype_matrix
+
 
 
 
